4a4a81052b5b3bbed3111c86a2ccea045d4ebbcb
Ian King <kickthechair@gmail.com>
2021-06-25 17:00:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,16 @@
 class Dog
   #setter (mutator) method - sets up or updates the value of the attribute
   def name=(dog_name)
+<<<<<<< HEAD
     @this_dogs_name = dog_name
+=======
+    this_dogs_name = dog_name
+>>>>>>> 056ba67f97beafd20eb5979ab27d5ac5cc1b03a4
   end
   
   #getter (accessor) method - this returns the value of the attribute
   def name
+<<<<<<< HEAD
     @this_dogs_name
   end
 end
@@ -15,3 +20,8 @@ lassie = Dog.new
 lassie.name = "Lassie"
 
 lassie.name
+=======
+    this_dogs_name
+  end
+end
+>>>>>>> 056ba67f97beafd20eb5979ab27d5ac5cc1b03a4
